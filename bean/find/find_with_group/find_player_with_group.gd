0a02fc6baefd79ce65aa8631e7978a692be051fc
Eloi Stree <eloistree@gmail.com>
2026-05-22 23:55:52 +0200

    Add example of find by group in scene

---
+++ bean/find/find_with_group/find_player_with_group.gd
@@ -0,0 +1,16 @@
+extends Node
+
+@export var player_group_name := "player"
+@export var node_found_in_group: Node3D
+
+func _ready() -> void:
+	node_found_in_group = get_tree().get_first_node_in_group(player_group_name) as Node3D
+	while true:
+		if node_found_in_group:
+			node_found_in_group.show()
+			await get_tree().create_timer(1).timeout
+			node_found_in_group.hide()
+			await get_tree().create_timer(1).timeout
+		
+		
+	
